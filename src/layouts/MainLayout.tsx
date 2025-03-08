37ed0b5f51e37b9814a4768023f9098130a08722
Gadza03 <mgadza@pmfst.hr>
2025-03-08 03:20:49 +0100

    Add animation

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,27 @@
 import Navbar from "../components/nav-bar/Navbar";
 import { Outlet } from "react-router";
+import { motion, AnimatePresence } from "framer-motion";
+
+const pageVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 0.7 } },
+};
+
 export default function MainLayout() {
   return (
-    <div>
+    <motion.div>
       <Navbar />
-      <Outlet />
-    </div>
+
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={window.location.pathname}
+          initial="initial"
+          animate="animate"
+          variants={pageVariants}
+        >
+          <Outlet />
+        </motion.div>
+      </AnimatePresence>
+    </motion.div>
   );
 }
