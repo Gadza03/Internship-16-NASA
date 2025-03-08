2a7f067751c7635d273e56dd28c9951327ebe700
Gadza03 <mgadza@pmfst.hr>
2025-03-08 03:41:38 +0100

    Hamburger menu

--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -2,9 +2,10 @@ import c from "../../styles/navbar.module.css";
 import nasaIcon from "../../assets/svgs/nasa-icon.svg";
 import { useNavigate } from "react-router";
 import ReactSwitch from "react-switch";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../contexts";
 import { paths } from "../../paths";
+import { FaXmark, FaBars } from "react-icons/fa6";
 
 const navLinks = [
   { name: "Home", path: paths.home },
@@ -15,18 +16,39 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const { isLight, toggleTheme } = useContext(ThemeContext);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleNavigatePath = (path: string) => {
+    navigate(path);
+    setMenuOpen(false);
+  };
 
   return (
     <nav className={c.navBar}>
       <div className={c.logo} onClick={() => navigate(paths.home)}>
         <img src={nasaIcon} alt="Nasa logo" />
       </div>
-      <ul className={c.linksWrapper}>
-        <ReactSwitch onChange={toggleTheme} checked={isLight} />
+      <div className={c.hamburger} onClick={toggleMenu}>
+        {!menuOpen ? (
+          <FaBars className={c.hamburgerIcon} />
+        ) : (
+          <FaXmark className={c.hamburgerIcon} />
+        )}
+      </div>
+
+      <ul className={`${c.linksWrapper} ${menuOpen ? c.openMenu : ""}`}>
+        <span>
+          Theme
+          <ReactSwitch onChange={toggleTheme} checked={isLight} />
+        </span>
         {navLinks.map(({ name, path }) => (
-          <li key={name} onClick={() => navigate(path)}>
+          <li key={name} onClick={() => handleNavigatePath(path)}>
             {name}
           </li>
         ))}
